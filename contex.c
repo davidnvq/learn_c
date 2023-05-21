6d603653ecd3023485fc1652aff7e561c0b73f23
Nguyen Van Quang <quanguet@gmail.com>
2023-05-21 16:06:44 +0900

    workable!

--- contex.c
+++ contex.c
@@ -3,6 +3,7 @@
 #include "image.h"
 #include "polygon.h"
 
+// Return the number of labels and store the labels in the array `labels` from the given image
 int ContourLabelsFromImage(tImage image, unsigned char Labels[])
 {
     if (image == NULL || image->matrix == NULL)
@@ -14,9 +15,11 @@ int ContourLabelsFromImage(tImage image, unsigned char Labels[])
     int NbCol = image->NbCol;
     tMatrix matrix = image->matrix;
 
+    // Maximum number of labels is 256, since the pixel values are in range 0-255
     int maxLabels = 256;
     int numLabels = 0;
 
+    // Create an array to store the label counts, default = 0
     unsigned char *labelCounts = (unsigned char *)calloc(maxLabels, sizeof(unsigned char));
     if (labelCounts == NULL)
     {
@@ -30,6 +33,7 @@ int ContourLabelsFromImage(tImage image, unsigned char Labels[])
         for (int j = 0; j < NbCol; j++)
         {
             unsigned char label = matrix[i][j];
+            // Only add the label if it is not 0 (background) and it is not already added (labelCounts[label] == 0)
             if (label != 0 && labelCounts[label] == 0)
             {
                 Labels[numLabels] = label;
@@ -52,36 +56,29 @@ int ContourLabelsFromImage(tImage image, unsigned char Labels[])
     return numLabels;
 }
 
-int ContourLabels(char FileNameLabels[], unsigned char Labels[])
+// Return the number of labels and store the labels in the array `labels` from the given pgm file
+int ContourLabels(char pgmFileName[], unsigned char labels[])
 {
-    tImage image = ImLire(FileNameLabels);
-    int num_labels = ContourLabelsFromImage(image, Labels);
+    tImage image = ImLire(pgmFileName);
+    int num_labels = ContourLabelsFromImage(image, labels);
     return num_labels;
 }
 
-tPolygon ContourSuivi(tImage Im, unsigned char label)
+// Return the next contour polygon for the given label
+tPolygon ContourSuivi(tImage image, unsigned char label)
 {
     int startX = -1, startY = -1;
     int x, y;
     int prev_x, prev_y;
 
-    for (int i = 0; i < Im->NbLig; i++)
-    {
-        for (int j = 0; j < Im->NbCol; j++)
-        {
-            printf("%d ", Im->matrix[i][j]);
-        }
-        printf("\n");
-    }
-
     // Find the starting pixel of the region
-    for (int i = 0; i < Im->NbLig; i++)
+    for (int i = 0; i < image->NbLig; i++)
     {
-        for (int j = 0; j < Im->NbCol; j++)
+        for (int j = 0; j < image->NbCol; j++)
         {
-            if (Im->matrix[i][j] == label)
+            if (image->matrix[i][j] == label)
             {
-                printf("Found label %d at (%d, %d)\n", label, i, j);
+                // printf("Found label %d at (%d, %d)\n", label, i, j);
                 startX = i;
                 startY = j;
                 break;
@@ -98,9 +95,8 @@ tPolygon ContourSuivi(tImage Im, unsigned char label)
         // Region not found, return NULL
         return NULL;
     }
-    printf("Start pixel: (%d, %d)\n", startX, startY);
 
-    int max_pixels = Im->NbLig * Im->NbCol;
+    int max_pixels = image->NbLig * image->NbCol;
     unsigned char *visited = (unsigned char *)calloc(max_pixels, sizeof(unsigned char));
 
     tPolygon contourPolygon = (tPolygon)malloc(sizeof(struct sPolygon));
@@ -113,60 +109,56 @@ tPolygon ContourSuivi(tImage Im, unsigned char label)
     prev_x = startX;
     prev_y = startY;
 
+    // printf("Add this vertex to contour polygon: (%d, %d) \n", x, y);
     polygon_add_vertex_at_end(x, y, contourPolygon);
-    display_polygon(contourPolygon);
 
+    // For the first vertex, we need to go left
     x = x - 1;
-
-    // printf("Current pixel 0: (%d, %d) px py (%d %d) \n", x, y, prev_x, prev_y);
 
     do
     {
-        int idx = x * Im->NbCol + y; // Index of the current pixel for visited array
+        int idx = x * image->NbCol + y; // Index of the current pixel for visited array
 
-        if (Im->matrix[x][y] == label)
+        // If we encounter a pixel/vertex which is the label, we need to go left
+        if (image->matrix[x][y] == label)
         {
-            if (visited[idx] == 1)
+            if (visited[idx] == 0)
             {
-                // printf("Already visited\n");
-                continue;
-            }
-            else
-            {
-                printf("updated Current pixel: (%d, %d) px py (%d %d) \n", x, y, prev_x, prev_y);
+                // printf("Add this vertex to contour polygon: (%d, %d) \n", x, y);
                 polygon_add_vertex_at_end(x, y, contourPolygon);
-                visited[idx] = 1;
+                visited[idx] = 1; // Mark as visited, so we don't add it again to the contour
             }
             if (x == prev_x)
             {
                 prev_x = x;
                 x = x + (prev_y - y);
                 prev_y = y;
-                printf("Left x\n");
+                // printf("Left x\n");
             }
             else
             {
                 prev_y = y;
                 y = y - (prev_x - x);
                 prev_x = x;
-                printf("Left y\n");
+                // printf("Left y\n");
             }
         }
-        else if (Im->matrix[x][y] != label)
+        // If we encounter a pixel/vertex which is not the label, we need to go right
+        else if (image->matrix[x][y] != label)
         {
             if (x == prev_x)
             {
                 prev_x = x;
                 x = x - (prev_y - y);
                 prev_y = y;
-                printf("Right x\n");
+                // printf("Right x\n");
             }
             else
             {
                 prev_y = y;
                 y = y + (prev_x - x);
                 prev_x = x;
-                printf("Right y\n");
+                // printf("Right y\n");
             }
         }
         else
@@ -177,13 +169,12 @@ tPolygon ContourSuivi(tImage Im, unsigned char label)
 
     } while (x != startX || y != startY);
 
-    printf("Final polygon:\n");
-    display_polygon(contourPolygon);
     return contourPolygon;
 }
 
 void ExtractContour(char *FileName, unsigned char Labels[], int NbRegions, char *ContourFileName)
 {
+    // Read the PGM file (open the pgm file to see the format!)
     tImage image = ImLire(FileName);
     FILE *file = fopen(ContourFileName, "a");
     fprintf(file, "%d\n", NbRegions);
@@ -191,49 +182,55 @@ void ExtractContour(char *FileName, unsigned char Labels[], int NbRegions, char 
     for (int i = 0; i < NbRegions; i++)
     {
         unsigned char label = Labels[i];
+        // Find the next contour polygon which has the label = Labels[i]
         tPolygon contourPolygon = ContourSuivi(image, label);
         if (contourPolygon != NULL)
         {
-            printf("Writing contour %d to file\n", i);
+            // printf("Writing contour %d to file\n", i);
+
+            // Write the contour polygon to the file, ONE at a time
             PolygonWrite(contourPolygon, file);
-            PolygonLiberer(contourPolygon);
+            PolygonLiberer(contourPolygon); // free the memory for the polygon
         }
     }
     fclose(file);
 }
 
-void ContourWriteOverImageSvg(char *ContourFileName, char *ImageFileName, int NbCol, int NbLig, tStyle *pStyle, char *SVGFileName)
+void ContourWriteOverImageSvg(char *ContourFileName, char *ImageFileName, int NbCol, int NbLig, tStyle *pStyle, char *SvgFileName)
 {
-    printf("Hello \n");
-    FILE *file = fopen(ContourFileName, "r");
-    FILE *svgFile = SvgCreate(SVGFileName, NbLig, NbCol);
+    FILE *contFile = fopen(ContourFileName, "r");
+    FILE *svgFile = SvgCreate(SvgFileName, NbLig, NbCol);
+
+    // Write the png image to the svg file first,
+    // so that the png image is at the back, and the contours are drawn on front!!!
     SvgWriteImage(svgFile, ImageFileName, NbCol, NbLig);
 
+    // Read the contours from the file and write them to the svg file
     int numRegions;
-    fscanf(file, "%d", &numRegions);
-    printf("Num regions: %d\n", numRegions);
+    fscanf(contFile, "%d", &numRegions);
+    // printf("Num regions: %d\n", numRegions);
 
     for (int i = 0; i < numRegions; i++)
     {
         tPolygon poly = PolygonCreate();
         int numVertices;
 
-        fscanf(file, "%d", &numVertices);
-        printf("Num vertices: %d\n", numVertices);
+        fscanf(contFile, "%d", &numVertices);
+        // printf("Num vertices: %d\n", numVertices);
 
         for (int i = 0; i < numVertices; i++)
         {
             int x, y;
-            fscanf(file, "%d %d", &x, &y);
-            // printf("Read x: %d y: %d\n", x, y);
+            fscanf(contFile, "%d %d", &x, &y);
             polygon_add_vertex_at_end(x, y, poly);
         }
-        printf("Write to MY GOD \n");
+        // write ONE polygon to the svg file at a time
         PolygonWriteSvg(poly, pStyle, svgFile);
-        PolygonLiberer(poly);
+        PolygonLiberer(poly); // free the memory allocated for the polygon after finish!
     }
-
-    SvgClose(file);
+    // Close the files
+    fclose(contFile);
+    SvgClose(svgFile);
     free(pStyle);
 }
 
@@ -244,31 +241,35 @@ int main(int argc, char *argv[])
         printf("Usage: %s <im2_pgm> <im2_png> <output_contour_file> <output_svg_file>\n", argv[0]);
         return 1;
     }
-
+    // Read command line arguments and store them in variables
+    // command line: ./contex  argv[1] argv[2] argv[3] argv[4]
+    // note that: argv[0] is the program name = ./contex
     char *pgmFile = argv[1];
     char *imgFile = argv[2];
     char *contourFile = argv[3];
     char *SVGFile = argv[4];
 
-    printf("pgmFile: %s\n", pgmFile);
-    printf("imgFile: %s\n", imgFile);
-    printf("contourFile: %s\n", contourFile);
-    printf("SVGFile: %s\n", SVGFile);
+    // printf("pgmFile: %s\n", pgmFile);
+    // printf("imgFile: %s\n", imgFile);
+    // printf("contourFile: %s\n", contourFile);
+    // printf("SVGFile: %s\n", SVGFile);
 
-    // Rest of your code goes here
+    // Find the number of labels in the image and store them in Labels
     unsigned char Labels[256];
     int numLabels = ContourLabels(pgmFile, Labels);
     printf("Number of labels: %d\n", numLabels);
 
     // print Labels array
+    printf("Label(s): ");
     for (int i = 0; i < numLabels; i++)
     {
-        printf("Label %d ", Labels[i]);
+        printf("%d ", Labels[i]);
     }
-    printf("\n");
+    printf("\n\n\n");
 
     tImage image = ImLire(pgmFile);
 
+    // default Style config from svg.h
     tStyle *Style = (tStyle *)malloc(sizeof(tStyle));
     Style->ColorTrait = "blue";
     Style->WidthTrait = 0.125;
@@ -277,8 +278,11 @@ int main(int argc, char *argv[])
     Style->OpacityFill = 0;
     Style->ColorPoints = "red";
     Style->RadiusPoints = 0.125;
+
+    // extract contour polygons and write them to ONE file (contourFile)
     ExtractContour(pgmFile, Labels, numLabels, contourFile);
 
+    // Read polygons from contourFile and write them to SVGFile with the image File
     ContourWriteOverImageSvg(contourFile, imgFile, image->NbCol, image->NbLig, Style, SVGFile);
     return 0;
 }
